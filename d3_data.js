7ce609b296140c60ce8710c5714e84b638dd1b8a
ohenneken <o.henneken@cynapps.nl>
2016-06-12 17:59:21 +0200

    Update d3_data.js
    adjusted url

--- d3_data.js
+++ d3_data.js
@@ -1,6 +1,6 @@
 window.addEvent('domready', function() {
 	new Request.HTML({
-		url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
+		url: '/gh/get/response.html/ohenneken/d3_data/tree/master/d3_data/',
 		data: {'delay': 1},
 		method: 'post',
 		update: 'demo',
